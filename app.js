815c72d20b593b3cb87284a23d1349c35279d3d4
Phuc Nguyen <kobaogiopo2@gmail.com>
2020-05-11 22:36:43 +0700

    init nodejs

---
+++ app.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const usersRouter = require('./routes/user.route');
+
+const app = express();
+const port = 3000
+app.set('view engine','pug');
+app.set('views',path.join(__dirname,'views'));
+// app.set('views','./views');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.get('/', (req,res)=>{
+    res.render('index',{message:'Good boy',age: 15});
+})
+
+app.use('/users',usersRouter);
+
+
+
+app.listen(port,()=>{
+    console.log(`Listening port ${port}`);
+})
---
+++ controller/users.controller.js
@@ -0,0 +1,43 @@
+
+let user = [
+        {name:"Phuong",id:1},
+        {name: 'Hong',id:2},
+        {name:"Tinh",id:3},
+        {name: 'Lo',id:4}
+]
+
+module.exports.index = (req,res) =>{
+    res.render('user',{user:user});
+};
+
+module.exports.search = (req,res) =>{
+    let q = req.query.nameUser.toLowerCase();
+    let matchSearch = user.filter(user =>{
+        return user.name.toLowerCase().indexOf(q) !== -1;
+    });
+    res.render('user',{user:matchSearch});
+};
+
+module.exports.create = (req,res) =>{
+    res.render('create');
+};
+
+module.exports.id =  (req,res) =>{
+    let idView = req.params.id;
+    let arrName = user.filter(element =>{
+        return element.id == idView;
+    });
+    let name = arrName[0].name;
+    res.send('<h1>Hello User '+name+'</h1>');
+};
+
+module.exports.postCreate = (req,res) =>{
+    if(!req.body.name){
+        res.render('create',{messageError: "Name can't empty"});
+    }else{
+        console.log(req.body);
+        user.push(req.body);
+        res.redirect('/users');
+    }
+    
+}
